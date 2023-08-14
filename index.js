0410400ae62feee6e9592014035688a389d8a57c
Samer Mobarak <samermobarak888@gmail.com>
2023-08-14 16:18:13 -0400

    scope_lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+//test 1
+var customerName = "bob"
+
+
+//test2 
+function upperCaseCustomerName() {
+      customerName = customerName.toUpperCase();
+}
+
+
+//test 3 
+
+function setBestCustomer() {
+      bestCustomer = 'not bob'
+}
+
+//test 4 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+//test 5
+
+const leastFavoriteCustomer = 'bobby';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "john"
+}
